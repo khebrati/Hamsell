d9f664db2c93e937caad3d0b404a58cfde25df83
Mohammad Erfan Khebrati <khebrati@gmail.com>
2024-07-09 11:26:28 +0330

    moved sign up to user controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace Hamsell.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly DbContext _context;
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -27,55 +26,7 @@ public class HomeController : Controller
     {
         return View();
     }
-    public IActionResult SignUp()
-    {
-        return View();
-    }
     
-    [HttpPost]
-    public async Task<IActionResult> SignUp(SignUpViewModel model)
-    {
-        // if (ModelState.IsValid)
-        // {
-        //     var existingAccount = await _context.Accounts
-        //         .FirstOrDefaultAsync(a => a.EmailAddress == model.EmailAddress);
-        //     if (existingAccount != null)
-        //     {
-        //         ModelState.AddModelError("EmailAddress", "Email already in use.");
-        //         return View(model);
-        //     }
-        //
-        //     var account = new Account
-        //     {
-        //         FirstName = model.FirstName,
-        //         LastName = model.LastName,
-        //         EmailAddress = model.EmailAddress,
-        //         PhoneNumber = model.PhoneNumber,
-        //         CreationDate = DateTime.UtcNow
-        //     };
-        //
-        //     _context.Accounts.Add(account);
-        //     await _context.SaveChangesAsync();
-        //
-        //     // Assuming automatic AccountId assignment upon saving
-        //     var user = new User
-        //     {
-        //         AccountId = account.AccountId,
-        //         // Additional properties like CityId and UserStatusId can be set here if needed
-        //     };
-        //
-        //     _context.Users.Add(user);
-        //     await _context.SaveChangesAsync();
-        //
-        //     // Implement any sign-in logic here
-        //
-        //     return RedirectToAction("Index", "Home"); // Redirect to a different action/view as appropriate
-        // }
-        //
-        // // If we got this far, something failed, redisplay form
-        // return View(model);
-        return View(model);
-    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
